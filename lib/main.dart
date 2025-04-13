0d3774bbe838eebb3aed664006775b2f544324a0
Evgeny Bazarov <roma111kaaa@mail.ru>
2025-04-13 18:14:48 +0600

    del readme

--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
-import 'package:match_3_game/src/game.dart';
-
-void main() {
-  runApp(GameWidget(game: Match3Game()));
-}
